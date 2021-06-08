2179b2ca38e7c0bd6e18d3cc0107565c6f2eb63f
Ben Arno <bdarno92@gmail.com>
2021-06-07 19:32:44 -0700

    halfway commit

---
+++ src/HornedBeast.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <p>I am Dave Arno, Website Author.</p>
+    );
+  }
+}
+
+export default Main;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HornedBeast from './HornedBeast.js';
+
+
+class Main extends React.Component {
+  render() {
+    return (
+      <p>I am Dave Arno, Website Author.</p>
+    );
+  }
+}
+
+export default Main;
