1984fb1ded4880c1014a19e8fd4ca78e4daa1616
Ben Arno <bdarno92@gmail.com>
2021-06-08 18:46:41 -0700

    finished feature 1

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,9 +4,9 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <>
-        <h2>{this.props.header}</h2>
-        <img src={this.props.src} alt={this.props.alt} title={this.props.title} />
-        <p>{this.props.para}</p>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.img} alt={this.props.alt} title={this.props.title} />
+        <p>{this.props.description}</p>
       </>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import HornedBeastData from './resources/HornedBeastData.json';
 
 
 class Main extends React.Component {
   render() {
+    const BeastList = HornedBeastData.map(i => <HornedBeast title={i.title} img={i.image_url} alt={i.keyword} description={i.description} />)
     return (
       <>
-        <HornedBeast header="Cow" src="https://thumbs-prod.si-cdn.com/pd4kPtn0yd2aA2Tr3if1nQxsMKA=/1072x720/filters:no_upscale()/https://public-media.si-cdn.com/filer/bc/13/bc13e43b-ff35-4de9-827d-c86bba8293b7/cow.jpg" alt="A cow staring pointedly at you." title="This is a cow" para="So cows are pretty cool, right?" />
-        <HornedBeast header="Narwhal" src="https://magazine.washington.edu/columns_wordpress/wp-content/uploads/2018/06/Narwhal375.jpg" alt="A narwhal swimming majestically through the sea." title="This is a Narwhal" para="Cows are less cool than narwhals" />
+        {BeastList}
       </>
     );
   }
